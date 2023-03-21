31fa598b7601448b2ccb3484c59e328c1dd7c990
shanelic_ <31277036+shanelic@users.noreply.github.com>
2023-03-21 17:20:08 +0800

    Initial Commit

---
+++ Test Wallet/Test_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_WalletApp.swift
+//  Test Wallet
+//
+//  Created by RBLabs RD - Shane on 2023/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
