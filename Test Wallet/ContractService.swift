bcf95516617273c50a21ccfe0b4ae4636ef136ba
Shane @ Red Building Labs <shanelic@rblabs.io>
2023-05-17 22:20:17 +0800

    feat: type to tell method should be called or sent

--- Test Wallet/ContractService.swift
+++ Test Wallet/ContractService.swift
@@ -192,21 +192,31 @@ public protocol BetterInvocation {
     /// - Parameter inputs: Input values. Must be in the correct order.
     /// - Returns: Invocation object
     func betterInvoke(_ inputs: [ABIEncodable]) -> SolidityInvocation
+    var type: SolidityFunctionType { get }
+}
+
+public enum SolidityFunctionType {
+    case constant
+    case payable
+    case nonPayable
 }
 
 extension SolidityConstantFunction: BetterInvocation {
+    public var type: SolidityFunctionType { .constant }
     public func betterInvoke(_ inputs: [ABIEncodable]) -> SolidityInvocation {
         return SolidityReadInvocation(method: self, parameters: inputs, handler: handler)
     }
 }
 
 extension SolidityNonPayableFunction: BetterInvocation {
+    public var type: SolidityFunctionType { .nonPayable }
     public func betterInvoke(_ inputs: [ABIEncodable]) -> SolidityInvocation {
         return SolidityNonPayableInvocation(method: self, parameters: inputs, handler: handler)
     }
 }
 
 extension SolidityPayableFunction: BetterInvocation {
+    public var type: SolidityFunctionType { .payable }
     public func betterInvoke(_ inputs: [ABIEncodable]) -> SolidityInvocation {
         return SolidityPayableInvocation(method: self, parameters: inputs, handler: handler)
     }
